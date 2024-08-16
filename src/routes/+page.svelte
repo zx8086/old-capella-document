c64e0fd31786c882f1811bec94843a22254ee9e1
7-Zark-7 <1085655+zx8086@users.noreply.github.com>
2024-08-16 06:31:33 +0200

    shadcn-svelte installation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { Button } from "$lib/components/ui/button";
+</script>
+
+<h1>Welcome to Capella Document Search</h1>
+<p>
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+
+<Button>Click me</Button>
